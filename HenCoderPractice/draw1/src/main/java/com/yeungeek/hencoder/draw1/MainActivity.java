445b111e18f51557f212c0df7d7edc6df9de2b5f
yeungeek <yangjian0410@gmail.com>
2018-07-23 10:01:04 +0800

    add hencoder

---
+++ HenCoderPractice/draw1/src/main/java/com/yeungeek/hencoder/draw1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.yeungeek.hencoder.draw1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author yeungeek
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
