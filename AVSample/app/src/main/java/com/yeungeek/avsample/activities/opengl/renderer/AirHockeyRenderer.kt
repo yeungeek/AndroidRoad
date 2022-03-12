eda4924b7be4c4936584cb5e202bff45a41f6ae4
yeungeek <yangjian0410@gmail.com>
2022-03-12 18:50:32 +0800

    [update]: update the float array

--- AVSample/app/src/main/java/com/yeungeek/avsample/activities/opengl/renderer/AirHockeyRenderer.kt
+++ AVSample/app/src/main/java/com/yeungeek/avsample/activities/opengl/renderer/AirHockeyRenderer.kt
@@ -5,18 +5,42 @@ import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import com.yeungeek.avsample.activities.opengl.helper.ShaderHelper
 import com.yeungeek.avsample.activities.opengl.helper.ShaderResReader
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
 class AirHockeyRenderer constructor(context: Context) : GLSurfaceView.Renderer {
+
     private var mContext: Context = context
 
     private val A_POSITION = "a_Position"
     private val U_COLOR = "u_Color"
+    private val POSITION_COMPONENT_COUNT = 2
+    private val BYTES_PER_FLOAT = 4
 
-    private var program: Int = 0
-    private var aPositionLocation: Int = 0
-    private var uColorLocation: Int = 0
+    private var program = 0
+    private var aPositionLocation = 0
+    private var uColorLocation = 0
+
+    private var vertexData: FloatBuffer? = null
+
+
+    init {
+        var tableVerticesWithTriangles = floatArrayOf(
+            //Triangle 1
+            -0.5f, -0.5f, -0.5f
+            -0.5f, -0.5f, 0.5f
+            -0.5f, -0.5f, 0.5f
+
+
+        )
+
+        vertexData = ByteBuffer.allocateDirect(BYTES_PER_FLOAT)
+            .order(ByteOrder.nativeOrder())
+            .asFloatBuffer()
+    }
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
         GLES20.glClearColor(1f, 0f, 0f, 0f)
@@ -45,6 +69,9 @@ class AirHockeyRenderer constructor(context: Context) : GLSurfaceView.Renderer {
 
         aPositionLocation = GLES20.glGetAttribLocation(program, A_POSITION)
         uColorLocation = GLES20.glGetUniformLocation(program, U_COLOR)
+
+        //5. Bind our data
+
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
