58de40db8bbaf72ad0ccbf535d69b63359a6d69e
yeungeek <yangjian0410@gmail.com>
2025-12-17 19:56:05 +0800

    [feat] add comopse

--- ComposeTutorial/app/src/main/java/com/yeungeek/composetutorial/MainActivity.kt
+++ ComposeTutorial/app/src/main/java/com/yeungeek/composetutorial/MainActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,17 +18,24 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.yeungeek.composetutorial.data.SampleData
 import com.yeungeek.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,12 +44,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeTutorialTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MessageCard(
-                        Message("Android", "Jetpack Compose"),
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+//                Scaffold {  }
+                Conversation(SampleData.conversationSample)
             }
         }
     }
@@ -61,7 +67,12 @@ fun MessageCard(msg: Message, modifier: Modifier = Modifier) {
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 msg.author,
                 color = MaterialTheme.colorScheme.secondary,
@@ -69,10 +80,14 @@ fun MessageCard(msg: Message, modifier: Modifier = Modifier) {
             )
             Spacer(modifier = Modifier.height(4.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp,
+                color =  surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)) {
                 Text(
                     msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.bodyMedium
                 )
             }
@@ -88,11 +103,28 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(name = "Light Mode")
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode")
 @Composable
-fun GreetingPreview() {
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+//@Preview(name = "Light Mode")
+//@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode")
+//@Composable
+//fun GreetingPreview() {
+//    ComposeTutorialTheme {
+//        MessageCard(Message("Android", "Jetpack Compose"))
+//    }
+//}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewConversation() {
     ComposeTutorialTheme {
-        MessageCard(Message("Android", "Jetpack Compose"))
+        Conversation(SampleData.conversationSample)
     }
 }
---
+++ ComposeTutorial/app/src/main/java/com/yeungeek/composetutorial/data/SampleData.kt
@@ -0,0 +1,77 @@
+package com.yeungeek.composetutorial.data
+
+import com.yeungeek.composetutorial.Message
+
+/**
+ * com.yeungeek.composetutorial.data.SampleData for Jetpack Compose Tutorial
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Lexi",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Lexi",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        Message(
+            "Lexi",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        Message(
+            "Lexi",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Lexi",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        Message(
+            "Lexi",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Lexi",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Lexi",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Lexi",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Lexi",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Lexi",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Lexi",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Lexi",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
